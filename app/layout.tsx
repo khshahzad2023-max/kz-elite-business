4c2e5a140286f3e387f3e278c32fc0d40ef9a35d
khshahzad2023-max <khshahzad2023@gmail.com>
2026-09-19 13:12:00 +0400

    Add Oman code to footer contact number

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({children}:{children:React.ReactNode}) {
               <div>
                 <h4>Contact</h4>
                 <p>Muscat, Sultanate of Oman<br/>
-                <a href="tel:+96878967229">78967229</a><br/>
+                <a href="tel:+96878967229">+968 78967229</a><br/>
                 <a href="tel:+96899248431">+968 99248431</a><br/>
                 info@kzelitebusiness.com<br/>
                 kzelitebusiness.com</p>
