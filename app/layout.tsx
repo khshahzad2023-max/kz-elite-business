d5f333f85f88ae9d4db7f12376057c24b37c9832
khshahzad2023-max <khshahzad2023@gmail.com>
2026-09-19 14:16:33 +0400

    Restore reliable CSS-only website intro

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import Link from "next/link";
 import ContactDock from "./ContactDock";
+import SiteIntro from "./SiteIntro";
 
 export const metadata = {
   title: "K&Z ELITE BUSINESS",
@@ -22,6 +23,7 @@ export default function RootLayout({children}:{children:React.ReactNode}) {
   return (
     <html lang="en">
       <body>
+        <SiteIntro />
         <header className="header">
           <div className="container navbar">
             <Link href="/" className="brand">
